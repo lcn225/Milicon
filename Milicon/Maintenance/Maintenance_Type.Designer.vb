78826d7cde3fb7ba422e9b0bae4b57c760c36e6b
LCN <lcn225@gmail.com>
2017-03-02 00:17:12 +0800

    增加规格种类维护功能

--- Milicon/Maintenance/Maintenance_Type.Designer.vb
+++ Milicon/Maintenance/Maintenance_Type.Designer.vb
@@ -22,16 +22,68 @@ Partial Class Maintenance_Type
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.type_DataGridView = New System.Windows.Forms.DataGridView()
+        Me.signin_Button = New System.Windows.Forms.Button()
+        Me.del_Button = New System.Windows.Forms.Button()
+        Me.exit_Button = New System.Windows.Forms.Button()
+        CType(Me.type_DataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'type_DataGridView
+        '
+        Me.type_DataGridView.AllowUserToDeleteRows = False
+        Me.type_DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.type_DataGridView.Location = New System.Drawing.Point(23, 46)
+        Me.type_DataGridView.Name = "type_DataGridView"
+        Me.type_DataGridView.RowTemplate.Height = 23
+        Me.type_DataGridView.Size = New System.Drawing.Size(464, 240)
+        Me.type_DataGridView.TabIndex = 0
+        '
+        'signin_Button
+        '
+        Me.signin_Button.Location = New System.Drawing.Point(23, 337)
+        Me.signin_Button.Name = "signin_Button"
+        Me.signin_Button.Size = New System.Drawing.Size(75, 23)
+        Me.signin_Button.TabIndex = 1
+        Me.signin_Button.Text = "Button1"
+        Me.signin_Button.UseVisualStyleBackColor = True
+        '
+        'del_Button
+        '
+        Me.del_Button.Location = New System.Drawing.Point(202, 337)
+        Me.del_Button.Name = "del_Button"
+        Me.del_Button.Size = New System.Drawing.Size(75, 23)
+        Me.del_Button.TabIndex = 2
+        Me.del_Button.Text = "Button2"
+        Me.del_Button.UseVisualStyleBackColor = True
+        '
+        'exit_Button
+        '
+        Me.exit_Button.Location = New System.Drawing.Point(412, 337)
+        Me.exit_Button.Name = "exit_Button"
+        Me.exit_Button.Size = New System.Drawing.Size(75, 23)
+        Me.exit_Button.TabIndex = 3
+        Me.exit_Button.Text = "Button3"
+        Me.exit_Button.UseVisualStyleBackColor = True
         '
         'Maintenance_Type
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(515, 408)
+        Me.Controls.Add(Me.exit_Button)
+        Me.Controls.Add(Me.del_Button)
+        Me.Controls.Add(Me.signin_Button)
+        Me.Controls.Add(Me.type_DataGridView)
         Me.Name = "Maintenance_Type"
         Me.Text = "Maintenance_Type"
+        CType(Me.type_DataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents type_DataGridView As DataGridView
+    Friend WithEvents signin_Button As Button
+    Friend WithEvents del_Button As Button
+    Friend WithEvents exit_Button As Button
 End Class
--- Milicon/Maintenance/Maintenance_Type.vb
+++ Milicon/Maintenance/Maintenance_Type.vb
@@ -1,3 +1,73 @@
-﻿Public Class Maintenance_Type
+﻿Imports System.Data.OleDb
 
+Public Class Maintenance_Type
+
+    Private Sub TextFormat()
+        Me.Text = ini.GetIniString("Maintenance_Type", "Title", "N/A")
+        Me.signin_Button.Text = ini.GetIniString("Maintenance_Type", "Button1", "N/A")
+        Me.del_Button.Text = ini.GetIniString("Maintenance_Type", "Button2", "N/A")
+        Me.exit_Button.Text = ini.GetIniString("Maintenance_Type", "Button3", "N/A")
+    End Sub
+    'UI文本初始化
+
+    Private Sub displayType()
+
+        DBcon()
+
+        Dim sql As String = "select * from ObjectType_List Order By Type_ID"
+        da = New OleDbDataAdapter(sql, cn)
+        ds = New DataSet
+        da.Fill(ds, "displayType")
+        '准备查询所有材料类型
+
+        Me.type_DataGridView.DataSource = ds.Tables(0)
+        Me.type_DataGridView.Columns(0).Visible = False
+        '隐藏第一列“ID”
+
+    End Sub
+
+    Private Sub Maintenance_Type_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ResetDGV(Me)
+        TextFormat()
+        displayType()
+
+    End Sub
+
+    Private Sub exit_Button_Click(sender As Object, e As EventArgs) Handles exit_Button.Click
+        Me.Close()
+    End Sub
+    '按退出按钮关闭窗口
+
+    Private Sub Maintenance_Type_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        MaintenanceMainMenu.Show()
+    End Sub
+
+    Private Sub signin_Button_Click(sender As Object, e As EventArgs) Handles signin_Button.Click
+
+        Dim num As Integer = Maintenance_ObjectList.DB_RowCount("ObjectType_List")
+        '统计DS内类型数
+
+        Dim num_now As Integer = Me.type_DataGridView.Rows.Count
+
+        For i = num To num_now - 2
+            If Me.type_DataGridView.Rows(i).Cells(0).ToString = "" Then
+                '判断是否填入新行
+                Exit For
+            Else
+                Dim tempStr As String = "INSERT INTO ObjectType_List (Obj_Type) VALUES ('" & Me.type_DataGridView.Rows(i).Cells(1).Value & "')"
+                'INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
+
+                Dim cmd As OleDbCommand = New OleDbCommand(tempStr, cn)
+                cn.Open()
+                cmd.ExecuteNonQuery()
+
+            End If
+
+        Next
+
+        MessageBox.Show("保存成功", "信息")
+        cn.Close()
+
+    End Sub
 End Class
